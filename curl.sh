60122605c06e80037281c7be5e01ffa25cf567bd
LiveG-Technologies <liveg2015@hotmail.com>
2017-08-08 18:06:26 +0100

    Removed prompt.

--- curl.sh
+++ curl.sh
@@ -2,18 +2,14 @@
 clear
 echo "Thank you for wanting to install Untangled 2017."
 echo "The installation is just under 25 megabytes."
-read -p "Install [y/n]? " REPLY
 
-if [ $REPLY == "y" ]
-then
-    clear
-    echo "Part 1 of 2."
-    git clone https://github.com/neontribe/untangled-2017.git
-    clear
-    echo "Part 2 of 2."
-    cd untangled-2017
-    sh install.sh
-    clear
-    echo "The installation has finished."
-    echo "Have fun!"
-fi
+clear
+echo "Part 1 of 2."
+git clone https://github.com/neontribe/untangled-2017.git
+clear
+echo "Part 2 of 2."
+cd untangled-2017
+sh install.sh
+clear
+echo "The installation has finished."
+echo "Have fun!"
