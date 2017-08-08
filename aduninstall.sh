0e20c6d04010ef1bdf06da28a09c01d1c2df9183
LiveG-Technologies <liveg2015@hotmail.com>
2017-08-08 12:41:00 +0100

    ADMIN 0.3.4

--- aduninstall.sh
+++ aduninstall.sh
@@ -7,10 +7,11 @@ echo "#!/bin/bash" > ../adinstall.sh
 echo "cd untangled-2017" >> ../adinstall.sh
 echo "clear" >> ../adinstall.sh
 echo "echo 'About to update... Hold on!'" >> ../adinstall.sh
+echo "git checkout Admin" >> ../adinstall.sh
 echo "git pull" >> ../adinstall.sh
 echo "cp player_example player_save" >> ../adinstall.sh
 echo "sh adinstall.sh" >> ../adinstall.sh
 clear
 echo "Sad to see you go."
 echo "Just run the following to come back:"
-echo "sh adinstall.sh"
+echo "sh ../adinstall.sh"
