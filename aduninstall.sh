b7181f9a995968664739b5b5c782dd1729a608b5
LiveG-Technologies <liveg2015@hotmail.com>
2017-08-08 12:30:40 +0100

    ADMIN 0.3.1

--- aduninstall.sh
+++ aduninstall.sh
@@ -3,7 +3,13 @@ clear
 echo "About to uninstall... Hold on!"
 git checkout master
 git pull
+echo "#!/bin/bash" > adinstall.sh
+echo "clear" >> adinstall.sh
+echo "About to update... Hold on!" >> adinstall.sh
+echo "git pull" >> adinstall.sh
+echo "cp player_example player_save" >> adinstall.sh
+echo "sh adinstall.sh" >> adinstall.sh
 clear
 echo "Sad to see you go."
 echo "Just run the following to come back:"
-echo "git checkout Admin & git pull & sh adinstall.sh"
+echo "sh adinstall.sh"
