26ff1fb486f2c95da2d0bd954380ee33d2899882
LiveG Technologies <liveg2015@hotmail.com>
2017-08-08 17:00:16 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -18,6 +18,7 @@ from enum import Enum
 from map import *
 from network import Network
 from player import *
+from flag import *
 from screen import MainMenu
 from level import SaveLevel
 from tile import Tileset
@@ -51,6 +52,7 @@ class GameClient():
         me = Player(self.screen, self.map, self.network)
 
         self.players = PlayerManager(me, self.network)
+        self.flags = [Flag(self.screen, self.map, "red"), Flag(self.screen, self.map, "blue")]
         self.map.set_centre_player(self.players.me)
         self.menu = MainMenu(self.screen, self.players)
 
@@ -73,7 +75,7 @@ class GameClient():
         pygame.event.set_allowed(None)
         pygame.event.set_allowed([pygame.locals.QUIT,
             pygame.locals.JOYAXISMOTION,
-            pygame.locals.KEYDOWN])
+            pygame.locals.KEYDOWN, pygame.locals.MOUSEBUTTONDOWN,  pygame.locals.JOYBUTTONDOWN])
 
         self.levels = {
             "main": SaveLevel('./assets/maps/CAPFLAG MAP')
@@ -104,6 +106,7 @@ class GameClient():
         toMove = False # Flag for when player moves - reduces network stress
         cast = False # Flag for when player casts spell.
         me = self.players.me
+        flags = self.flags
 
         if me.mute == "False":
             LevelMusic.play_music_repeat()
@@ -146,32 +149,49 @@ class GameClient():
                             self.set_state(GameState.MENU)
 
                         elif event.type == pygame.locals.KEYDOWN:
-                            if event.key == pygame.locals.K_UP:
+                            if event.key == pygame.locals.K_UP or event.key == pygame.locals.K_w:
                                 me.move(Movement.UP)
                                 last_direction = Movement.UP
                                 toMove = True
-                            elif event.key == pygame.locals.K_DOWN:
+                            elif event.key == pygame.locals.K_DOWN or event.key == pygame.locals.K_s:
                                 me.move(Movement.DOWN)
                                 last_direction = Movement.DOWN
                                 toMove = True
-                            elif event.key == pygame.locals.K_LEFT:
+                            elif event.key == pygame.locals.K_LEFT or event.key == pygame.locals.K_a:
                                 me.move(Movement.LEFT)
                                 last_direction = Movement.LEFT
                                 toMove = True
-                            elif event.key == pygame.locals.K_RIGHT:
+                            elif event.key == pygame.locals.K_RIGHT or event.key == pygame.locals.K_d:
                                 me.move(Movement.RIGHT)
                                 last_direction = Movement.RIGHT
                                 toMove = True
-                            elif event.key == pygame.locals.K_RETURN:
+                            elif event.key == pygame.locals.K_RETURN or event.key == pygame.locals.K_SPACE :
                                 cast = True
                                 me.attack(Action.SPELL, last_direction)
+                                
+                            if event.key == pygame.locals.K_r and me.can_step_ability:
+                                me.step = 2
+                                me.steptime = time.time()
+                                me.can_step_ability = False
+                               
                             pygame.event.clear(pygame.locals.KEYDOWN)
+                            
+                        if time.time() - me.steptime >30:
+                            me.can_step_ability = True
+                        elif time.time() - me.steptime >3:
+                            me.step = 1
+                    if pygame.mouse.get_pressed()[0]:
+                        cast = True
+                        me.attack(Action.SPELL, last_direction)
+                        pygame.event.clear(pygame.locals.MOUSEBUTTONDOWN)  
+                    
 
                     # https://stackoverflow.com/a/15596758/3954432
                     # Handle controller input by setting flags (move, neutral)
                     # and using timers (delay, pressed).
                     # Move if pressed timer is greater than delay.
-                    if(pygame.joystick.get_count() > 0 and not me.name.startswith("windows")):
+
+                    if(pygame.joystick.get_count() > 0 and not me.name.startswith("windows") and not toMove):
                         joystick = pygame.joystick.Joystick(0)
                         move = False
                         delay = 100
@@ -212,16 +232,25 @@ class GameClient():
                                 me.move(Movement.LEFT)
                                 last_direction = Movement.LEFT
                                 toMove = True
-                        # A
-                        if joystick.get_button(1):
+
+                        # R
+                        if joystick.get_button(5):
+
                             cast = True
                             me.attack(Action.SPELL, last_direction)
+                        if joystick.get_button (9):
+                            self.set_state(GameState.MENU)
+
                         last_update = pygame.time.get_ticks()
 
                     self.map.render()
                     me.render()
+                    for flag in flags:
+                        flag.render()
                     for spell in me.cast_spells:
                         spell.render()
+                    for particle in me.particle_list:
+                        particle.render()
 
                     self.players.set(self.network.node.peers())
 
@@ -264,6 +293,11 @@ class GameClient():
                                     if event.type == "SHOUT":
                                         team_defs = bson.loads(event.msg[0])
                                         self.players.set_teams(team_defs)
+                                if event.group == "ctf:flags":
+                                    if event.type == "SHOUT":
+                                        flag_defs = bson.loads(event.msg[0])
+                                        self.flags[0].set_position_from_authority(flag_defs.get("red"))
+                                        self.flags[1].set_position_from_authority(flag_defs.get("blue"))
 
                                 if network_player:
                                     network_player.set_position(Position(**new_position))
