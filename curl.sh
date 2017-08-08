c1b6f37e8bae7a295e20ecf6468967aaba2356be
LiveG-Technologies <liveg2015@hotmail.com>
2017-08-08 18:13:52 +0100

    ADMIN 0.4.1

---
+++ curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Thank you for wanting to install ADMIN."
+
+clear
+echo "Part 1 of 2."
+git checkout Admin
+git pull
+clear
+echo "Part 2 of 2."
+sh adinstall.sh
+clear
+echo "The installation has finished."
+echo "Have fun!"
