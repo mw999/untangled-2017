2c85b6bbc11adabc9693459efd1538bbb929eb15
LiveG-Technologies <liveg2015@hotmail.com>
2017-08-09 11:44:29 +0100

    ADMIN 0.4.7

--- adinstall.sh
+++ adinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "You're all set! Welcome to ADMIN 0.4.6!"
+echo "You're all set! Welcome to ADMIN 0.4.7!"
 echo "Now for your name. What is it?"
 read name
 echo [Player] > player_save
